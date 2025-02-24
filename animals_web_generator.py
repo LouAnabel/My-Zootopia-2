35d892fcf3cc0ed105b9fdf9cbdd5ca242e1ffda
louisedebatin <loudebatin@gmail.com>
2025-02-24 15:44:14 +0100

    added an exception an loop for the user input

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -3,6 +3,28 @@ import requests
 
 
 
+def get_user_input():
+    user_input = input("Please enter the animal you want to search for: ")
+    return user_input
+
+def get_info_from_API(api_url, user_animal):
+    response = requests.get(api_url, headers={'X-Api-Key': 'q9r48ssh7l6bFIW9jifEIQ==L3BwmkN4TTAnt5Qg'})
+    if response.status_code == requests.codes.ok:
+        animals = response.json()
+        return animals
+    else:
+        error_message = "Error:", response.status_code, response.text
+        return error_message
+
+def get_info_from_API(api_url, user_animal):
+    response = requests.get(api_url, headers={'X-Api-Key': 'q9r48ssh7l6bFIW9jifEIQ==L3BwmkN4TTAnt5Qg'})
+    if response.status_code == requests.codes.ok:
+        animals = response.json()
+        return animals
+    else:
+        print("Error:", response.status_code, response.text)
+
+
 """creates the string of all the info for each 
 animal that will be added to the json and displayed on the website"""
 def serialize_animal(animal):
@@ -20,39 +42,38 @@ def serialize_animal(animal):
     output += "</li>"
     return output
 
-def get_user_input():
-    user_input = input("Please enter the animal you want to search for: ")
-    return user_input
-
-def get_info_from_API(api_url, user_animal):
-    response = requests.get(api_url, headers={'X-Api-Key': 'q9r48ssh7l6bFIW9jifEIQ==L3BwmkN4TTAnt5Qg'})
-    if response.status_code == requests.codes.ok:
-        animals = response.json()
-        return animals
-    else:
-        error_message = "Error:", response.status_code, response.text
-        return error_message
 
 def main():
-    user_animal = get_user_input()
-    api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(user_animal)
-
     with open("animals_template.html", 'r', encoding='utf-8') as html_file:
         html_content = html_file.read()
 
-    animals_data = get_info_from_API(api_url, user_animal)
-    print("Website was successfully generated to the file animals.html.")
-    print(animals_data)
+    while True:
+        try:
+            user_animal = get_user_input()
+            api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(user_animal)
+
+            animals_data = get_info_from_API(api_url, user_animal)
+            if len(animals_data) > 0:
+                output = ''
+                for animal in animals_data:
+                    output += serialize_animal(animal)
+
+                html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
+
+                with open("animals.html", 'w') as new_file:
+                    new_file.write(html_content)
+
+                print("\nWebsite was successfully generated to the file animals.html.")
+                break
+            else:
+                print()
+                raise ValueError(f"Error: Unfortunately {user_animal} was not found in the list of animals. Try again.")
+                continue
+
+        except ValueError as e:
+            print(e)
 
 
-    output = ''
-    for animal in animals_data:
-        output += serialize_animal(animal)
-
-    html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
-
-    with open("animals.html", 'w') as new_file:
-        new_file.write(html_content)
 
 if __name__ == "__main__":
     main()
