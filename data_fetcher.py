69f2bb050851ddb7bae0307086bc79264baee255
louisedebatin <loudebatin@gmail.com>
2025-02-25 12:21:38 +0100

    added KEY_API constant

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,8 +1,9 @@
-import json
 import requests
 
+API_KEY = 'q9r48ssh7l6bFIW9jifEIQ==L3BwmkN4TTAnt5Qg'
+
 def fetch_data(api_url, animal_name):
-    response = requests.get(api_url, headers={'X-Api-Key': 'q9r48ssh7l6bFIW9jifEIQ==L3BwmkN4TTAnt5Qg'})
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
     if response.status_code == requests.codes.ok:
         animals = response.json()
         print(animals)
@@ -10,5 +11,4 @@ def fetch_data(api_url, animal_name):
     else:
         error_message = "Error:", response.status_code, response.text
         return error_message
-
 
