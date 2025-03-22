4ef59dea831c654a493e88fedefacbe3db2f474f
louisedebatin <loudebatin@gmail.com>
2025-03-22 12:52:05 +0100

    final changes for regrade

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,14 +1,16 @@
 from data_fetcher import fetch_data
-# or in main: animals_data = data_fetcher.fetch_data(animal_name)
 
 
 def get_user_input():
     user_input = input("Please enter the animal you want to search for: ")
     return user_input
 
-"""creates the string of all the info for each 
-animal that will be added to the json and displayed on the website"""
+
 def serialize_animal(animal):
+    """creates the string of all the info for each
+    animal that will be added to the json and displayed on the website"""
+    print(animal)
+    print(type(animal))
     output = ' '
     output += "<li class='cards__item'>\n"
     output += f"<div class='card__title'>{animal.get('name', 'Unknown')}</div><br/>\n"
@@ -25,32 +27,30 @@ def serialize_animal(animal):
 
 
 def main():
+
     with open("animals_template.html", 'r', encoding='utf-8') as html_file:
         html_content = html_file.read()
 
         user_animal = get_user_input()
-        api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(user_animal)
+        api_url = f'https://api.api-ninjas.com/v1/animals?name={user_animal}'
 
         animals_data = fetch_data(api_url, user_animal)
+        print(f"animals_data: {animals_data}")
+        print(type(animals_data))
+
         if len(animals_data) > 0:
             output = ''
             for animal in animals_data:
                 output += serialize_animal(animal)
-
-            html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
-
-            with open("animals.html", 'w') as new_file:
-                new_file.write(html_content)
             print("\nWebsite was successfully generated to the file animals.html.")
-
         else:
-            error_message = f"<h2>Error! The animal {user_animal} doesn't exist. Please check if it's correctly written and in english!</h2>"
-            html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", error_message)
-
-            with open("animals.html", 'w') as new_file:
-                new_file.write(html_content)
-
+            output = f"<h2>Error! The animal {user_animal} doesn't exist. Please check if it's correctly written and in english!</h2>"
             print(f"Error: Unfortunately {user_animal} was not found in the list of animals. Try again.")
+
+        # Replace content and write to file - common code for both scenarios
+        html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
+        with open("animals.html", 'w', encoding='utf-8') as new_file:
+            new_file.write(html_content)
 
 
 if __name__ == "__main__":
--- data_fetcher.py
+++ data_fetcher.py
@@ -1,12 +1,13 @@
-import os
-from dotenv import load_dotenv
 import requests
+from dotenv import load_dotenv
+import os
 
 #loading variables from .env
 load_dotenv()
 
 #access environment variables from .env file
 API_KEY = os.getenv('API_KEY')
+
 
 def fetch_data(api_url, animal_name):
     response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
